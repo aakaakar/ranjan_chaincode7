dcec5e0cdf12ae0b230ddb97e9827dbe325f9409
Aakar <aakaakar@gmail.com>
2017-05-31 14:41:01 +0530

    Changed write method name

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -113,8 +113,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {
 		return t.Init(stub, "init", args)
-	} else if function == "write" {
-		return t.write(stub, args)
+	} else if function == "testRanjan" {
+		return t.testRanjanMethod(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
@@ -136,7 +136,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 }
 
 // write - invoke function to write key/value pair
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleChaincode) testRanjanMethod(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, value string
 	var err error
 	fmt.Println("running write()")
