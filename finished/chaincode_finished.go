1bb5b9709031abc8f7e78b031a95182c6e2148e5
Aakar <aakaakar@gmail.com>
2017-05-31 23:34:41 +0530

    modified code to write data

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -30,33 +30,6 @@ func main() {
 
 	fmt.Println("****** Starting to send information to my ledger")
 
-	type data struct {
-		ID   string `json:"tax_id"`
-		Name string `json:"name"`
-	}
-	testData := &data{
-		ID:   "1453",
-		Name: "constantinople",
-	}
-
-	fmt.Println("****** Starting to create my client")
-
-	client, error := cloudant.NewClient("attaidifieveredgedatingi", "85363815ee8b0396585f5cf7d3a12508aba2a3d2")
-
-	fmt.Println("****** Completed creating my client, ensuring db exists")
-
-	fmt.Println("**** Error while creating client: " + error.Error())
-
-	client.CreateDB("https://ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix:9d794d83dc3913e7958a6ce546293269aab45ef097d5610b6eb43b9c6bf0bd7e@ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix.cloudant.com")
-
-	fmt.Println("****** Completed ensuring db exists, creating DB object with url- https://ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix:9d794d83dc3913e7958a6ce546293269aab45ef097d5610b6eb43b9c6bf0bd7e@ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix.cloudant.com")
-
-	dbObj := client.DB("https://ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix:9d794d83dc3913e7958a6ce546293269aab45ef097d5610b6eb43b9c6bf0bd7e@ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix.cloudant.com")
-
-	fmt.Println(" ******** got DB object dbObj, creating document by calling dbObj.CreateDocument(testData)")
-
-	dbObj.CreateDocument(testData)
-
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
@@ -69,35 +42,6 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	}
 
 	err := stub.PutState("hello_world", []byte(args[0]))
-
-	fmt.Println("****** Starting to send information to my ledger")
-
-	type data struct {
-		ID   string `json:"tax_id"`
-		Name string `json:"name"`
-	}
-	testData := &data{
-		ID:   "1453",
-		Name: "constantinople",
-	}
-
-	fmt.Println("****** Starting to create my client")
-
-	client, error := cloudant.NewClient("attaidifieveredgedatingi", "85363815ee8b0396585f5cf7d3a12508aba2a3d2")
-
-	fmt.Println("****** Completed creating my client, ensuring db exists")
-
-	fmt.Println("**** Error while creating client: " + error.Error())
-
-	client.CreateDB("https://ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix:9d794d83dc3913e7958a6ce546293269aab45ef097d5610b6eb43b9c6bf0bd7e@ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix.cloudant.com")
-
-	fmt.Println("****** Completed ensuring db exists, creating DB object with url- https://ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix:9d794d83dc3913e7958a6ce546293269aab45ef097d5610b6eb43b9c6bf0bd7e@ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix.cloudant.com")
-
-	dbObj := client.DB("https://ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix:9d794d83dc3913e7958a6ce546293269aab45ef097d5610b6eb43b9c6bf0bd7e@ab5e5a7c-76de-4d8a-8516-64e21e8c4042-bluemix.cloudant.com")
-
-	fmt.Println(" ******** got DB object dbObj, creating document by calling dbObj.CreateDocument(testData)")
-
-	dbObj.CreateDocument(testData)
 
 	if err != nil {
 		return nil, err
@@ -113,8 +57,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {
 		return t.Init(stub, "init", args)
-	} else if function == "testRanjan" {
-		return t.testRanjanMethod(stub, args)
+	} else if function == "lumos" {
+		return t.myMethod(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
@@ -136,18 +80,10 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 }
 
 // write - invoke function to write key/value pair
-func (t *SimpleChaincode) testRanjanMethod(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleChaincode) myMethod(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
 	var key, value string
 	var err error
-	fmt.Println("running write()")
-
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
-	}
-
-	key = args[0] //rename for funsies
-	value = args[1]
-	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
 
 	fmt.Println("****** Starting to send information to my ledger")
 
@@ -177,6 +113,16 @@ func (t *SimpleChaincode) testRanjanMethod(stub shim.ChaincodeStubInterface, arg
 	fmt.Println(" ******** got DB object dbObj, creating document by calling dbObj.CreateDocument(testData)")
 
 	dbObj.CreateDocument(testData)
+
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
 
 	if err != nil {
 		return nil, err
